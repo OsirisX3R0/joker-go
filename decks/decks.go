920d7bf7e8d4561d7be0534a889553b9f366ed27
Richard Harris <richardharris916@gmail.com>
2021-04-16 07:30:10 -0500

    Added decks package

---
+++ decks/decks.go
@@ -0,0 +1,28 @@
+package decks
+
+import "github.com/OsirisX3R0/joker-go/cards"
+
+type Deck struct {
+	draw    Pile
+	discard Pile
+}
+
+type Pile []cards.Card
+
+type DeckOptions struct {
+	numberOfDecks int
+	jokers        bool
+}
+
+func New() Deck {
+	var blankPile Pile
+
+	return Deck{
+		draw:    blankPile,
+		discard: blankPile,
+	}
+}
+
+func (d *Deck) Push(c cards.Card) {
+	d.draw = append(d.draw, c)
+}
